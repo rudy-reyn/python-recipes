b1f549a8005de86898c4f6f469e1d5a7c8ae7f80
Rudy Reynolds <rure6748@colorado.edu>
2022-03-27 15:21:20 -0600

    updated infix.py; now uses a BaseInfix abstract base class and includes a infix decorator for easy subclassing.

--- recipes/__init__.py
+++ recipes/__init__.py
@@ -2,4 +2,5 @@ from .tuples import Tuple, fields, asdict, items
 from .guards import Guard, guard
 from .typed import Typed, typed
 from .pipefunc import pipefunc, PipeFunc, StdinPipe
-from .infix import Infix, infix
+from .infix import BaseInfix, Infix, infix, new_infix, operator_method
+from .utils import Nil
