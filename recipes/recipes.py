979fde660567b72fc7aa58dc19fe33b57b198c22
Rudy Reynolds <rure6748@colorado.edu>
2022-04-11 19:15:16 -0600

    updated recipes.curry and tests

---
+++ recipes/recipes.py
@@ -0,0 +1,96 @@
+# 04/02/22
+# recipes.py
+import  inspect
+from functools import wraps, partial
+from typing import Any, Callable
+
+__all__ = "pipefunc", "curry"
+
+class Function:
+    __slots__ = "_func", "_signature"
+
+    def __init__(self, *args, **kwargs):
+        name = type(self).__name__
+        message = f"{name} cannot be instantiated without defining an __init__ method"
+        raise TypeError(message)
+
+    def __repr__(self):
+        name = type(self).__name__
+        func = self.func.__name__
+        if name.endswith("y"):
+            name = name[:-1] + "ied"
+        return f"<{name} function {func} at {id(self):#x}>"
+
+    def __call__(self, *arg, **kwargs) -> Any:
+        return self.func(*arg, **kwargs)
+
+    @staticmethod
+    def _require_callable(func: Callable) -> None:
+        if not callable(func):
+            raise TypeError(f"Function must be callable, got type {type(func)}")
+
+    @property
+    def func(self) -> Callable:
+        if not hasattr(self, "_func"):
+            raise AttributeError("function has not been set")
+        self._require_callable(self._func)
+        return self._func
+
+    @func.setter
+    def func(self, func: Callable):
+        self._require_callable(func)
+        self._func = func
+
+class pipefunc(Function):
+    """A decorator for creating bash or R style function pipes.
+
+    >>> @pipefunc
+    >>> def factorial(n, a=1):
+    ...     if n <= 0:
+    ...         return a
+    ...     return factorial(n - 1, n * a)
+    ...
+    >>> 5 | factorial
+    120
+    """
+    __slots__ = "_func",
+
+    def __init__(self, func: Callable):
+        self.func = func
+
+    def __ror__(self, arg: Any) -> Any:
+        return self(arg)
+
+
+class curry(Function):
+    """A decorator for currying functions.
+
+    >>> @curry
+    >>> def add(a, b, c, d=0):
+    ...     return a + b + c
+    ...
+    >>> add(5, 15)
+    <curried function add at 0x{id}>
+    >>> add(100)
+    120
+    """
+
+    def __init__(self, func, *args, **kwargs):
+        self.func = partial(func, *args, **kwargs)
+        self._signature = inspect.signature(self.func)
+        self.func.__name__ = func.__name__
+
+    def __call__(self, *args, **kwargs):
+        try:
+            bound = self._signature.bind(*args, **kwargs)
+        except TypeError as bind_terr:
+            try:
+                partial_binding = self._signature.bind_partial(*args, **kwargs)
+            except TypeError as terr:
+                traceback = terr.with_traceback(None)
+                message = f"Could not bind function arguments for {self}: {traceback}"
+                raise TypeError(message)
+            else:
+                return type(self)(self.func, *args, **kwargs)
+        else:
+            return self.func(*args, **kwargs)
--- recipes/tests/recipes.py
+++ recipes/tests/recipes.py
@@ -1,6 +1,7 @@
 # 04/02/22
 # recipes.py
 import sys
+import re
 import pytest
 from .. import pipefunc, curry
 
@@ -12,20 +13,6 @@ def factorial():
             return a
         return factorial(n - 1, n * a)
     return factorial
-
-@pytest.fixture
-def add():
-    @curry
-    def add(a, b, c=0):
-        return a + b + c
-    return add
-
-@pytest.fixture
-def add_positional():
-    @curry
-    def add(a, b, c, /):
-        return a + b + c
-    return add
 
 def test_pipefunc(factorial):
     assert repr(factorial)
@@ -43,33 +30,106 @@ def test_pipefunc(factorial):
     assert (5 | factorial) == 120
 
 @pytest.fixture
+def generic_message():
+    return r"Could not bind function arguments for .*:"
+
+@pytest.fixture
+def curry_repr():
+    return re.compile(r"<curried function <?\w+>? at 0x[a-f0-9]+>")
+
+@pytest.fixture
 def curry_helper():
-    def helper(function):
-        assert repr(function)
-        assert isinstance(function, curry)
+    def helper(func):
+        return bool(repr(func)) and isinstance(func, curry)
     return helper
 
-def test_curry(add, add_positional, curry_helper):
+def test_repr(curry_repr):
+    func = repr(curry(lambda a, b: (a, b)))
+
+    @repr
+    @curry
+    def func1(a, b, /):
+        return a, b
+
+    @repr
+    @curry
+    def func2(*, a, b):
+        return a, b
+
+    assert curry_repr.match(func), func
+    assert curry_repr.match(func1), func1
+    assert curry_repr.match(func2), func2
+
+def test_curry(curry_helper):
+    @curry
+    def add(a, b, c):
+        return a + b + c
+
     with pytest.raises(TypeError):
         curry(None)
+
+    assert curry_helper(add), add
+    assert curry_helper(add(1)), add
+
+    assert add(1, 2, 3) == add(1, 2)(3) == add(1)(2, 3) == add(1)(2)(3) == add(a=1, b=2, c=3) == 6
+
+    with pytest.raises(TypeError) as exc_info:
+        add(1, 2, 3, 4, 5)
+
+    add1 = add(1)
+    assert curry_helper(add1)
+    add3 = add1(2)
+    assert curry_helper(add3)
+    assert add3(c=3) == 6
+
+def test_curry_pos_only(curry_helper, generic_message):
+    @curry
+    def add(a, b, c, /):
+        return a + b + c
+
     assert curry_helper(add)
     assert curry_helper(add(1))
-    assert add(1)(2)(3) == 6
+    assert curry_helper(add(1, 2))
+    assert add(1, 2, 3) == 6
 
-def test_curry_pos_only(add_positional, curry_helper):
-    assert curry_helper(add_positional)
-    assert curry_helper(add_positional(1))
-    assert curry_helper(add_positional(1, 2))
-    assert add_positional(1, 2, 3) == 6
+    msg = "parameter is positional only, but was passed as a keyword"
+    pos_only_message = lambda letter: f"{generic_message} '{letter}' {msg}"
 
-    with pytest.raises(TypeError):
-        add_positional(1, 2, c=3)
+    expected = pos_only_message("c")
 
-    with pytest.raises(TypeError):
-        add_positional(a=1, b=2, c=3)
+    with pytest.raises(TypeError, match=expected) as exc_info:
+        add(1, 2, c=3)
 
-    add1 = add_positional(1)
+    expected = pos_only_message("a")
+    with pytest.raises(TypeError, match=expected) as exc_info:
+        add(a=1, b=2, c=3)
+
+    add1 = add(1)
     assert curry_helper(add1)
     add3 = add1(2)
     assert curry_helper(add3)
     assert add3(3) == 6
+
+def test_keyword_only(curry_helper, generic_message):
+    @curry
+    def add(*, a, b, c,):
+        return a + b + c
+
+    assert curry_helper(add)
+    assert curry_helper(add(a=1))
+    assert curry_helper(add(a=1, b=2))
+    assert add(a=1, b=2, c=3) == 6
+
+    expected = f"{generic_message} too many positional arguments"
+
+    with pytest.raises(TypeError, match=expected) as exc_info:
+        add(1, 2, 3)
+
+    with pytest.raises(TypeError, match=expected) as exc_info:
+        add(1, b=2, c=3)
+
+    add1 = add(a=1)
+    assert curry_helper(add1)
+    add3 = add1(b=2)
+    assert curry_helper(add3)
+    assert add3(c=3) == 6
